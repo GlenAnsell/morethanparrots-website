617d30ea6f1fcff46887c6c60c5d31a73f84f905
Glen Ansell <glen@xcode.co.za>
2026-07-06 22:07:13 +0200

    Phase 1 i18n: add LocaleMiddleware, LANGUAGES, i18n URLs, language switcher, wrap base/home/book_detail/contact/about templates

--- mtp_website/urls.py
+++ mtp_website/urls.py
@@ -3,6 +3,7 @@ from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 from django.http import HttpResponse
 from blog.sitemaps import BlogSitemap
 
@@ -20,14 +21,21 @@ def robots_txt(request):
     return HttpResponse('\n'.join(lines), content_type='text/plain')
 
 
+# Non-i18n URLs (sitemap, robots, admin, etc.)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('book.urls')),
-    path('marketing/', include('marketing.urls')),
-    path('blog/', include('blog.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('robots.txt', robots_txt, name='robots_txt'),
 ]
+
+# i18n URLs with language prefix
+urlpatterns += i18n_patterns(
+    path('', include('book.urls')),
+    path('marketing/', include('marketing.urls')),
+    path('blog/', include('blog.urls')),
+    prefix_default_language=True,
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
