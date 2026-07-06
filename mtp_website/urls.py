e167265f85751f8fc72b82d8537e8aaaa519e795
Glen Ansell <glen@xcode.co.za>
2026-07-06 12:56:37 +0200

    SEO: add meta descriptions, canonical URLs, Open Graph, robots.txt, homepage title

--- mtp_website/urls.py
+++ mtp_website/urls.py
@@ -3,11 +3,22 @@ from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 from blog.sitemaps import BlogSitemap
 
 sitemaps = {
     'blog': BlogSitemap,
 }
+
+
+def robots_txt(request):
+    lines = [
+        'User-agent: *',
+        'Allow: /',
+        'Sitemap: https://morethanparrots.com/sitemap.xml',
+    ]
+    return HttpResponse('\n'.join(lines), content_type='text/plain')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,6 +26,7 @@ urlpatterns = [
     path('marketing/', include('marketing.urls')),
     path('blog/', include('blog.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', robots_txt, name='robots_txt'),
 ]
 
 if settings.DEBUG:
